53468d3619ba054c7487085971e166b29f17bbfa
dimawi <124430243+dimawi@users.noreply.github.com>
2023-02-04 15:19:38 +0600

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -20,7 +20,7 @@ public class Circle {
         return 2 * Math.PI * radius ;
     }
     public String toString(){
-        return "Circle[ radius = " + radius + " ]";
+        return "Circle[radius = " + radius + "]";
     }
 
 }
